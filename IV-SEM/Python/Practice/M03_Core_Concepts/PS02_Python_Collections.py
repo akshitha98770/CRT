ba413a6d253aa525fa89bfc2baa341af75afb2f8
Akshitha <ningampallyakshitha@gmail.com>
2026-03-11 09:26:09 +0530

    Assinments

---
+++ IV-SEM/Python/Practice/M03_Core_Concepts/PS02_Python_Collections.py
@@ -0,0 +1,35 @@
+'''
+#1) creating a list
+a = [10,20,30,40,50]
+print(a)
+b = list(10,20,312)
+print(b)
+
+#2)Accessing of list
+a = [10,20,30,40,50]
+print(a[0])
+print(a[-1])
+
+#3)Creating list with repeated elements
+ a = [10,20,30,40,50]
+print(a * 2)
+ 
+#4)Adding elements to a list
+a = [10,20,30,40,50]
+a.append(100)
+print(a)
+a.insert(1,50)
+print(a)
+
+#5)Removing elements from list
+a = [10,20,30,40,50]
+print(a)
+a.pop()
+print(a)
+
+#6)slicing
+a = [10,20,30,40,50]
+print(a[0:])
+print(a[2:]) 
+'''
+#7)
