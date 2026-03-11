e4a4906c162ee2459615f2b4983c59451ec56fc9
Akshitha <ningampallyakshitha@gmail.com>
2026-03-11 21:57:08 +0530

    Assignments

--- IV-SEM/Python/Assignments/M02_Logic_Building_and_Patterns/AST01/task.py
+++ IV-SEM/Python/Assignments/M02_Logic_Building_and_Patterns/AST01/task.py
@@ -1,7 +1,16 @@
 def count_digits(n: int) -> int:
-    n = abs(n)          
-    return len(str(n)) 
+    n = abs(n)  # handle negative numbers
+    if n == 0:
+        return 1
+    
+    count = 0
+    while n > 0:
+        n //= 10
+        count += 1
+    
+    return count
 
-if __name__ == "__main__":
+
+if __name__ == '__main__':
     n = int(input())
     print(count_digits(n))
